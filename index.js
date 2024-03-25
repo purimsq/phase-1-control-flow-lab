c63cab86c5c1e22b00f0e964a6f1c0d290f36b9a
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-03-26 00:28:54 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+ function scuberGreetingForFeet(ride){
   // Write your code here!
+  let output = "";
+
+  if(ride > 2500){
+    output = "No can do.";
+  }else if(ride > 2000 && ride < 2500){
+    output = "I will gladly take your thirty bucks.";
+  }else if(ride > 400 && ride < 2000){
+    output = "That will be twenty bucks.";
+  }else{
+    output = "This one is on me!";
+  };
+
+  return output
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
